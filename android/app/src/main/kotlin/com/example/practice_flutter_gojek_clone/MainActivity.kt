b6a0700e21d2d1686f8fdbfbbeecbb57ef59d956
Fatichul Iksan <fatichul.iksan@gmail.com>
2020-05-30 17:57:11 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/practice_flutter_gojek_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practice_flutter_gojek_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/launcher/launcher_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class LauncherPage extends StatefulWidget {
+  @override
+  _LauncherPageState createState() => new _LauncherPageState();
+}
+
+class _LauncherPageState extends State<LauncherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
